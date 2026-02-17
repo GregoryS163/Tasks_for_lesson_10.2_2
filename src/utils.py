91aca839512193350b960d4763fa5a79f7c72253
Gregory Salnikov <greengo86@mail.ru>
2026-02-17 20:34:43 +0400

    fixed func calculate_tax

--- src/utils.py
+++ src/utils.py
@@ -15,12 +15,16 @@ def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
     return taxed_prices
 
 
-def calculate_tax(price: float, tax_rate: float) -> float:
-    if price < 0:
+def calculate_tax(price: float, tax_rate: float, discount=0, round_cost=2) -> float:
+    if not isinstance(price, int | float) or not isinstance(tax_rate, int | float):
+        raise TypeError('неверный тип аргумента')
+    elif price < 0:
         raise ValueError('Неверная цена')
 
     elif tax_rate < 0 or tax_rate >= 100:
         raise ValueError('Неверный налоговый процент')
 
     else:
-        return price + (price * tax_rate / 100)
+        taxed_price = price + (price * tax_rate / 100)
+        discount_price = taxed_price - (taxed_price * discount / 100)
+        return round(discount_price, round_cost)
--- tests/test_calculate_tax.py
+++ tests/test_calculate_tax.py
@@ -4,15 +4,36 @@ from src.utils import calculate_tax
 
 
 @pytest.mark.parametrize('price, tax_rate, expected_result', [
-    (50, 10, 55.0),
-    (100, 20, 120.0),
-    (0, 22, 0)
+    (50, 7, 53.5),
+    (100, 20, 120),
+    (0, 22, 0),
 ])
 def test_calculate_tax_basic(price, tax_rate, expected_result):
     assert calculate_tax(price, tax_rate) == expected_result
 
 
-def test_calculate_tax_invalid_args():
+@pytest.mark.parametrize('price, tax_rate, discount, expected_result', [
+    (50, 7, 7, 49.76),
+    (100, 20, 10, 108),
+    (0, 22, 0, 0),
+    (181, 8.5, 10, 176.75)
+])
+def test_calculate_tax_with_discount(price, tax_rate, discount, expected_result):
+    assert calculate_tax(price, tax_rate, discount) == expected_result
+
+
+@pytest.mark.parametrize('price, tax_rate, discount, round_cost, expected_result', [
+    (87, 4, 10, 1, 81.4),
+    (50, 7, 7.5, 3, 49.487),
+    (100, 20.55, 10.5, 4, 107.8923),
+    (0, 22, 0, 1, 0),
+    (181, 8.5, 10, 0, 177)
+])
+def test_calculate_tax_with_all_args(price, tax_rate, discount, round_cost, expected_result):
+    assert calculate_tax(price, tax_rate, discount, round_cost) == expected_result
+
+
+def test_calculate_tax_invalid_value_args():
     with pytest.raises(ValueError) as negative_price:
         calculate_tax(-11, 10)
     assert str(negative_price.value) == 'Неверная цена'
@@ -28,3 +49,22 @@ def test_calculate_tax_invalid_args():
     with pytest.raises(ValueError) as tax_rate_more_100:
         calculate_tax(30, 101)
     assert str(tax_rate_more_100.value) == 'Неверный налоговый процент'
+
+
+def test_calculate_tax_invalid_type_args():
+    with pytest.raises(TypeError) as invalid_type:
+        calculate_tax('120', 15)
+    assert str(invalid_type.value) == 'неверный тип аргумента'
+
+    with pytest.raises(TypeError) as invalid_type:
+        calculate_tax(120, [12])
+    assert str(invalid_type.value) == 'неверный тип аргумента'
+
+    with pytest.raises(TypeError) as invalid_type:
+        calculate_tax({}, 10)
+    assert str(invalid_type.value) == 'неверный тип аргумента'
+
+    with pytest.raises(TypeError) as invalid_type:
+        calculate_tax('50', (10,))
+    assert str(invalid_type.value) == 'неверный тип аргумента'
+
